36974097bbc56d48267d3af129b285e9c34a78ac
sherif Ashraf Roshdy <77529268+sherifRoshdy@users.noreply.github.com>
2021-08-05 16:57:56 +0200

    new train

---
+++ write the text alone/js/main.js
@@ -0,0 +1,58 @@
+//window.onload = document.getElementById("test").innerHTML="thank";
+
+//function more1()
+//{
+//    document.getElementById("test").innerHTML="The committee challenges the investigation of marks, supportive supervision, confinement, and innovative mediations. The World Health Organization (WHO) has announced the discharge of COVID-19 to be a general public health dilemma of interest (PHEIC) on 30 January 2020 and a pandemic on 11 March 2020. Social synchromesh of the infection happened in all six WHO areas in the greatest countries[5].";
+//}
+//function more2()
+//{
+//    document.getElementById("test").innerHTML="In computer science, in contradiction to the common intelligence that humans and animals exhibit, artificial intelligence ( AI), also named machine intelligence, is the intelligence that computers presentation. The advanced artificial intelligence textbook describes the department as the knowledge of energetic clients, any practice that acknowledges its surroundings and practices moves that enhance its possibility of fortuitously accomplishing its aims.";
+//}
+///////////////////////////////////////////////////////////////
+//var but1=document.getElementById("b1"),
+//    
+//    but2=document.getElementById("b2"),
+//    
+//    temp=document.getElementById("test");
+//
+//but1.onclick=function()
+//{
+//    temp.innerHTML="The committee challenges the investigation of marks, supportive supervision, confinement, and innovative mediations. The World Health Organization (WHO) has announced the discharge of COVID-19 to be a general public health dilemma of interest (PHEIC) on 30 January 2020 and a pandemic on 11 March 2020. Social synchromesh of the infection happened in all six WHO areas in the greatest countries[5].";
+//}
+//but2.onclick=function()
+//{
+//    temp.innerHTML="In computer science, in contradiction to the common intelligence that humans and animals exhibit, artificial intelligence ( AI), also named machine intelligence, is the intelligence that computers presentation. The advanced artificial intelligence textbook describes the department as the knowledge of energetic clients, any practice that acknowledges its surroundings and practices moves that enhance its possibility of fortuitously accomplishing its aims.";
+//    
+//}
+
+var tex=document.getElementById("tex1"),
+    temp=document.getElementById("test");
+tex.onkeyup=function(){
+    temp.innerHTML=tex.value;
+    
+}
+
+//var tex=document.getElementById("tex1"),
+//    temp=document.getElementById("test");
+//tex.onkeydown=function(){
+//    temp.innerHTML=tex.value;
+//    
+//}
+
+//var tex=document.getElementById("tex1"),
+//    temp=document.getElementById("test");
+//tex.onkeypress=function(){
+//    temp.innerHTML=tex.value;
+//    
+//}
+
+
+
+
+
+
+
+
+
+
+
